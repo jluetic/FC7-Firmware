01ce5c6e81aa2d18c49bd50e1bb83fab70095d4b
Mykyta Haranko <portwarez@gmail.com>
2017-03-20 13:40:13 +0100

    2cbc2 support added [8cbc2_dev] - separate triggers for each cbc are not routed - dc-dc clock is not routed - lvds lines are not routed

--- fw/src/usr/phy/fmcio_mapping/fmcio_mapping_one_hybrid.vhd
+++ fw/src/usr/phy/fmcio_mapping/fmcio_mapping_one_hybrid.vhd
@@ -58,27 +58,32 @@ architecture rtl of fmcio_mapping_one_hybrid is
     -- 8cbc2 signals
     signal signals_from_8cbc2    : std_logic_vector(fmc_8cbc2_in_id.na-1 downto 0);
     signal signals_to_8cbc2      : std_logic_vector(fmc_8cbc2_out_id.na-1 downto 0);
+    
+    -- 2cbc2 signals
+    signal signals_from_2cbc2    : std_logic_vector(fmc_2cbc2_in_id.na-1 downto 0);
+    signal signals_to_2cbc2      : std_logic_vector(fmc_2cbc2_out_id.na-1 downto 0);
 
 begin
 
 gen_cbc2: if CBC_VERSION = 2 generate
-    gen_8cbc2: if FMC_TYPE = FMC_8CBC2 generate
     
-        clk40_oddr : oddr
-        generic map(
-            ddr_clk_edge    => "opposite_edge",
-            init            => '0',
-            srtype          => "sync"
-        )
-        port map (
-            q               => clk40_i_oddr,
-            c               => clk40_i,
-            ce              => '1',
-            d1              => '1',
-            d2              => '0',
-            r               => '0',
-            s               => '0'
-            );
+    clk40_oddr : oddr
+    generic map(
+        ddr_clk_edge    => "opposite_edge",
+        init            => '0',
+        srtype          => "sync"
+    )
+    port map (
+        q               => clk40_i_oddr,
+        c               => clk40_i,
+        ce              => '1',
+        d1              => '1',
+        d2              => '0',
+        r               => '0',
+        s               => '0'
+        );
+
+    gen_8cbc2: if FMC_TYPE = FMC_8CBC2 generate
     
         --===========================================--
         -- Buffers for 8CBC2
@@ -86,12 +91,19 @@ gen_cbc2: if CBC_VERSION = 2 generate
         --===========================================--
             -- input
             fmc_la_ibuf_gen: if io_map_8cbc2(i).fmc_dir = "i" generate
-                gen_good_polarity: if NOT io_map_8cbc2(i).wrong_pol generate
-                    fmc_la_ibufds : ibufds generic map(  IOSTANDARD => "LVDS_25", DIFF_TERM => TRUE ) port map( o => signals_from_8cbc2(io_map_8cbc2(i).sig_id), i => fmc_la_p(i), ib => fmc_la_n(i) );                                                                                                                        
-                end generate gen_good_polarity;
-                gen_wrong_polarity: if io_map_8cbc2(i).wrong_pol generate
-                    fmc_la_ibufds : ibufds generic map(  IOSTANDARD => "LVDS_25", DIFF_TERM => TRUE ) port map( o => signals_from_8cbc2(io_map_8cbc2(i).sig_id), i => not fmc_la_p(i), ib => not fmc_la_n(i) );                                                                                                                        
-                end generate gen_wrong_polarity;
+                
+                dummy_lvds_ibuf_gen: if io_map_8cbc2(i).sig_id = fmc_8cbc2_in_id.na generate
+                    fmc_la_ibufds : ibufds generic map(  IOSTANDARD => "LVDS_25", DIFF_TERM => TRUE ) port map( o => open, i => fmc_la_p(i), ib => fmc_la_n(i) );
+                end generate dummy_lvds_ibuf_gen;
+            
+                sig_lvds_ibuf_gen: if io_map_8cbc2(i).sig_id /= fmc_8cbc2_in_id.na generate
+                    gen_good_polarity: if NOT io_map_8cbc2(i).wrong_pol generate
+                        fmc_la_ibufds : ibufds generic map(  IOSTANDARD => "LVDS_25", DIFF_TERM => TRUE ) port map( o => signals_from_8cbc2(io_map_8cbc2(i).sig_id), i => fmc_la_p(i), ib => fmc_la_n(i) );                                                                                                                        
+                    end generate gen_good_polarity;
+                    gen_wrong_polarity: if io_map_8cbc2(i).wrong_pol generate
+                        fmc_la_ibufds : ibufds generic map(  IOSTANDARD => "LVDS_25", DIFF_TERM => TRUE ) port map( o => signals_from_8cbc2(io_map_8cbc2(i).sig_id), i => not fmc_la_p(i), ib => not fmc_la_n(i) );                                                                                                                        
+                    end generate gen_wrong_polarity;
+                end generate sig_lvds_ibuf_gen;
             end generate fmc_la_ibuf_gen;    
         
             -- output
@@ -143,6 +155,67 @@ gen_cbc2: if CBC_VERSION = 2 generate
         signals_to_8cbc2(fmc_8cbc2_out_id.l1a) <= fast_cmd_cbc2_i.trigger;
         
     end generate gen_8cbc2;
+    
+    gen_2cbc2: if FMC_TYPE = FMC_2CBC2 generate    
+       
+        --===========================================--
+        -- Buffers for 2CBC2
+        fmcpin_map_gen : for i in 0 to 33 generate
+        --===========================================--
+            -- input
+            fmc_la_ibuf_gen: if io_map_2cbc2(i).fmc_dir = "i" generate
+            
+                dummy_lvds_ibuf_gen: if io_map_2cbc2(i).sig_id = fmc_2cbc2_in_id.na generate
+                    fmc_la_ibufds : ibufds generic map(  IOSTANDARD => "LVDS_25", DIFF_TERM => TRUE ) port map( o => open, i => fmc_la_p(i), ib => fmc_la_n(i) );
+                end generate dummy_lvds_ibuf_gen;
+                
+                sig_lvds_ibuf_gen: if io_map_2cbc2(i).sig_id /= fmc_2cbc2_in_id.na generate
+                    gen_good_polarity: if NOT io_map_2cbc2(i).wrong_pol generate
+                        fmc_la_ibufds : ibufds generic map(  IOSTANDARD => "LVDS_25", DIFF_TERM => TRUE ) port map( o => signals_from_2cbc2(io_map_2cbc2(i).sig_id), i => fmc_la_p(i), ib => fmc_la_n(i) );                                                                                                                        
+                    end generate gen_good_polarity;
+                    gen_wrong_polarity: if io_map_2cbc2(i).wrong_pol generate
+                        fmc_la_ibufds : ibufds generic map(  IOSTANDARD => "LVDS_25", DIFF_TERM => TRUE ) port map( o => signals_from_2cbc2(io_map_2cbc2(i).sig_id), i => not fmc_la_p(i), ib => not fmc_la_n(i) );                                                                                                                        
+                    end generate gen_wrong_polarity;
+                end generate sig_lvds_ibuf_gen;
+            end generate fmc_la_ibuf_gen;    
+        
+            -- output
+            fmc_la_obuf_gen: if io_map_2cbc2(i).fmc_dir = "o" generate
+        
+                dummy_lvds_obuf_gen: if io_map_2cbc2(i).sig_id = fmc_2cbc2_out_id.na generate
+                    fmc_la_obufds : obufds generic map( IOSTANDARD => "LVDS_25" ) port map( o => fmc_la_p(i), ob => fmc_la_n(i), i => '0' );
+                end generate dummy_lvds_obuf_gen;
+                
+                sig_lvds_obuf_gen: if io_map_2cbc2(i).sig_id /= fmc_2cbc2_out_id.na generate
+                    gen_good_polarity: if NOT io_map_2cbc2(i).wrong_pol generate                                                             
+                        fmc_la_obufds : obufds generic map( IOSTANDARD => "LVDS_25" ) port map( o => fmc_la_p(i), ob => fmc_la_n(i), i => signals_to_2cbc2(io_map_2cbc2(i).sig_id) );
+                    end generate gen_good_polarity; 
+                    
+                    gen_wrong_polarity: if io_map_2cbc2(i).wrong_pol generate                                                             
+                        fmc_la_obufds : obufds generic map( IOSTANDARD => "LVDS_25" ) port map( o => fmc_la_p(i), ob => fmc_la_n(i), i => not signals_to_2cbc2(io_map_2cbc2(i).sig_id) );
+                    end generate gen_wrong_polarity;
+                end generate sig_lvds_obuf_gen;
+        
+            end generate fmc_la_obuf_gen;    
+        end generate fmcpin_map_gen;
+        --===========================================--
+        
+        SDA_miso_o <= signals_from_2cbc2(fmc_2cbc2_in_id.sda_from_cbc);
+        CBC_lines_o(0)(0) <= signals_from_2cbc2(fmc_2cbc2_in_id.cbc0_trigdata);
+        CBC_lines_o(0)(1) <= signals_from_2cbc2(fmc_2cbc2_in_id.cbc0_stubdata);
+        CBC_lines_o(1)(0) <= signals_from_2cbc2(fmc_2cbc2_in_id.cbc1_trigdata);
+        CBC_lines_o(1)(1) <= signals_from_2cbc2(fmc_2cbc2_in_id.cbc1_stubdata);
+        
+        signals_to_2cbc2(fmc_2cbc2_out_id.clk) <= clk40_i_oddr;
+        signals_to_2cbc2(fmc_2cbc2_out_id.scl) <= SCL_i;
+        signals_to_2cbc2(fmc_2cbc2_out_id.sda_to_cbc) <= SDA_mosi_i;
+        signals_to_2cbc2(fmc_2cbc2_out_id.hard_reset) <= reset_i;
+        signals_to_2cbc2(fmc_2cbc2_out_id.fast_reset) <= fast_cmd_cbc2_i.fast_reset;
+        signals_to_2cbc2(fmc_2cbc2_out_id.i2c_refresh) <= fast_cmd_cbc2_i.i2c_refresh;
+        signals_to_2cbc2(fmc_2cbc2_out_id.test_pulse) <= fast_cmd_cbc2_i.test_pulse_trigger;
+        signals_to_2cbc2(fmc_2cbc2_out_id.l1a) <= fast_cmd_cbc2_i.trigger;
+        
+    end generate gen_2cbc2;
 end generate gen_cbc2;
 
 gen_cbc3: if CBC_VERSION = 3 generate
--- fw/src/usr/phy/fmcio_mapping/fmcio_package.vhd
+++ fw/src/usr/phy/fmcio_mapping/fmcio_package.vhd
@@ -45,6 +45,98 @@ end record;
 type fmcpin_sig_mapset_type is array ( 0 to 33 ) of fmcpin_sig_map_type;
 
 --====================================--
+-- 2CBC2 Hybrid Electrical
+--====================================--
+type fmc_2cbc2_in_id_type is
+record
+    sda_from_cbc  : natural;
+    cbc0_trigdata : natural;
+    cbc0_stubdata : natural;
+    cbc1_trigdata : natural;
+    cbc1_stubdata : natural;
+    na            : natural;
+end record;
+
+type fmc_2cbc2_out_id_type is
+record		
+    clk                  : natural;
+    scl                  : natural;
+    sda_to_cbc           : natural;
+    hard_reset    : natural;
+    fast_reset    : natural;
+    i2c_refresh   : natural;
+    test_pulse    : natural;
+    l1a           : natural;
+    na		             : natural;
+end record;
+
+constant fmc_2cbc2_in_id : fmc_2cbc2_in_id_type :=
+( 	sda_from_cbc  =>  0,
+    cbc0_trigdata =>  1,
+    cbc0_stubdata =>  2,
+    cbc1_trigdata =>  3,
+    cbc1_stubdata =>  4,
+    na            =>  5 
+);
+
+constant fmc_2cbc2_out_id : fmc_2cbc2_out_id_type :=		
+(	clk                => 0,
+    scl                => 1,
+    sda_to_cbc         => 2,
+    hard_reset  => 3,
+    fast_reset  => 4,
+    i2c_refresh => 5,
+    test_pulse  => 6,
+    l1a         => 7,
+    na                 => 8 
+);
+
+constant io_map_2cbc2    : fmcpin_sig_mapset_type :=
+(
+    ( fmc_dir => "i",   fmcpin_id   =>  0,   sig_id  =>  fmc_2cbc2_in_id.cbc1_trigdata,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  1,   sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "i",   fmcpin_id   =>  2,   sig_id  =>  fmc_2cbc2_in_id.cbc1_stubdata,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  3,   sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "i",   fmcpin_id   =>  4,   sig_id  =>  fmc_2cbc2_in_id.na,    wrong_pol => false ), -- should be trigger from cbc 1 (in)
+    ( fmc_dir => "o",   fmcpin_id   =>  5,   sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  6,   sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "i",   fmcpin_id   =>  7,   sig_id  =>  fmc_2cbc2_in_id.cbc0_trigdata,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  8,   sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  9,   sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  10,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),    
+    ( fmc_dir => "i",   fmcpin_id   =>  11,  sig_id  =>  fmc_2cbc2_in_id.cbc0_stubdata,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  12,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  13,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  14,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  15,  sig_id  =>  fmc_2cbc2_out_id.l1a,    wrong_pol => false ),    
+    ( fmc_dir => "i",   fmcpin_id   =>  16,  sig_id  =>  fmc_2cbc2_in_id.na,    wrong_pol => false ), -- should be trigger from cbc 0 (in)
+    ( fmc_dir => "o",   fmcpin_id   =>  17,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  18,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),    
+    ( fmc_dir => "o",   fmcpin_id   =>  19,  sig_id  =>  fmc_2cbc2_out_id.test_pulse,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  20,  sig_id  =>  fmc_2cbc2_out_id.fast_reset,    wrong_pol => false ),    
+    ( fmc_dir => "o",   fmcpin_id   =>  21,  sig_id  =>  fmc_2cbc2_out_id.i2c_refresh,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  22,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),    
+    ( fmc_dir => "o",   fmcpin_id   =>  23,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),    
+    ( fmc_dir => "o",   fmcpin_id   =>  24,  sig_id  =>  fmc_2cbc2_out_id.clk,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  25,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ), -- should be clock to dc-dc converter 1mhz (out)   
+    ( fmc_dir => "o",   fmcpin_id   =>  26,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),    
+    ( fmc_dir => "o",   fmcpin_id   =>  27,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  28,  sig_id  =>  fmc_2cbc2_out_id.sda_to_cbc,    wrong_pol => false ),
+    ( fmc_dir => "i",   fmcpin_id   =>  29,  sig_id  =>  fmc_2cbc2_in_id.sda_from_cbc,    wrong_pol => false ),    
+    ( fmc_dir => "o",   fmcpin_id   =>  30,  sig_id  =>  fmc_2cbc2_out_id.hard_reset,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  31,  sig_id  =>  fmc_2cbc2_out_id.scl,    wrong_pol => false ),
+    ( fmc_dir => "o",   fmcpin_id   =>  32,  sig_id  =>  fmc_2cbc2_out_id.na,    wrong_pol => false ), -- lvds data out (test)
+    ( fmc_dir => "i",   fmcpin_id   =>  33,  sig_id  =>  fmc_2cbc2_in_id.na,    wrong_pol => false )  -- lvds data in (test)
+);
+
+constant i2c_address_map_2cbc2: i2c_address_map_type(0 to 1) := 
+(
+    0 =>  "1000001",
+    1 =>  "1000010"
+);
+--====================================--
+
+--====================================--
 -- 8CBC2 Hybrid Electrical
 --====================================--
 type fmc_8cbc2_in_id_type is
@@ -196,12 +288,6 @@ record
     reset                : natural;
     na		             : natural;
 end record;
-
-constant i2c_address_map_2cbc2: i2c_address_map_type(0 to 1) := 
-(
-    0 =>  "1000001",
-    1 =>  "1000010"
-);
 --====================================--
 
 constant i2c_address_map_2cbc3: i2c_address_map_type(0 to 1) := 
